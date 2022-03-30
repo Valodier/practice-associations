b30a434176f86f6094eaf8c7d4f3df995561c78e
William Meyer <valodier@icloud.com>
2022-03-30 17:24:30 -0400

    Adds appointment model/table, migrates meetings/speakers/appointments tables

---
+++ db/migrate/20220330212330_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[7.0]
+  def change
+    create_table :appointments do |t|
+      t.integer :speaker_id
+      t.integer :meeting_id
+
+      t.timestamps
+    end
+  end
+end
